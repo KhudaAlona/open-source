8abc40328ad59728d073f1458216f6abd74716ff
Aliona <alionkakhuda28@gmail.com>
2025-04-15 16:29:56 +0300

    Role implementation(Create a new user and etc.)

---
+++ src/main/java/edu/pzks/opensource/SecurityBean.java
@@ -0,0 +1,31 @@
+package edu.pzks.opensource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+
+
+import java.util.Collection;
+import java.util.Map;
+
+@Configuration
+public class SecurityBean {
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private  String issuerUri;
+
+    @Bean
+    public JwtDecoder jwtDecoder(OAuth2ResourceServerProperties properties) {
+        return JwtDecoders.fromOidcIssuerLocation(issuerUri);
+    }
+
+
+
+}
--- src/main/java/edu/pzks/opensource/SecurityConfig.java
+++ src/main/java/edu/pzks/opensource/SecurityConfig.java
@@ -3,6 +3,7 @@ package edu.pzks.opensource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -12,6 +13,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
@@ -25,7 +27,7 @@ public class SecurityConfig {
         http
                 .oauth2ResourceServer( auth ->
                         auth.jwt( jwt ->
-                                jwt.jwtAuthenticationConverter(new JwtAuthenticationConverter()))
+                                jwt.jwtAuthenticationConverter(new KeycloakJwtAuthenticationConverter()))
                 );
 
         http
